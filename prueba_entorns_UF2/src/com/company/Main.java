f7897c44500648ed8d85676a621cba894251bf7a
martinh118 <94359581+martinh118@users.noreply.github.com>
2022-01-10 10:28:12 +0100

    Add files via upload

---
+++ prueba_entorns_UF2/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * @version 1.1
+ * @author martinh
+ */
+public class Main {
+
+    /**
+     * 
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo!");
+    }
+}
